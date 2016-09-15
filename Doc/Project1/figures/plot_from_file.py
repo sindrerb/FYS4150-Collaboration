7d32724cb8ffce40ed5f7ddc669e3c2645754002
sindrerb <s.r.bilden@fys.uio.no>
2016-09-15 15:30:13 +0200

    added figures

--- Doc/Project1/figures/plot_from_file.py
+++ Doc/Project1/figures/plot_from_file.py
@@ -3,13 +3,10 @@ from scipy.integrate import quad
 import numpy as np
 import os
 
-#paths
-my_path = os.path.abspath(r"C:\Users\Heine\Desktop\Skole\FYS4150_Comp_Fys\FYS4150\Exercise_1\plot")
-os.chdir(r"C:\Users\Heine\Desktop\Skole\FYS4150_Comp_Fys\FYS4150\Exercise_1\plot")
 
-file = np.loadtxt("10e6task_c.txt", dtype = 'float', skiprows = 2, usecols = (0,1,2))
+file = np.loadtxt("../programs/build_d/run10", dtype = 'float', skiprows = 2, usecols = (0,1,2))
 
-file_lenght = str(len(file))
+file_lenght = str(len(file)-2)
 file_header = "Number of iterations " + file_lenght
 #print file , file_lenght
 
@@ -19,12 +16,12 @@ fig = figure()
 grid(True, which = 'both')#, ls= '-')
 grid('on')
 
-plot(file[:,0],file[:,1],'r-', label = '$approx. solutin$ ') 
-plot(file[:,0],file[:,2],'b-', label = '$exact solution$ ') 
+plot(file[:,0],file[:,1],'r-', label = '$approx.\ solution$ ') 
+plot(file[:,0],file[:,2],'b-', label = '$exact\ solution$ ') 
 
 xlabel(r'$x$', size = 20,labelpad= 5 )
 ylabel(r'$f(x)$',size = 20, labelpad = 5)
 title(file_header)
-legend(loc='smart')
-fig.savefig('10e6ex_c.png')
+legend()
+fig.savefig('b_run10.png')
 show()
