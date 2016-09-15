90c8174019bf0be05a712d6ca6ce658f79b53b31
sindrerb <s.r.bilden@fys.uio.no>
2016-09-15 19:39:04 +0200

    Updated figures

--- Doc/Project1/figures/plot_from_file.py
+++ Doc/Project1/figures/plot_from_file.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 
 
-file = np.loadtxt("../programs/build_d/run10", dtype = 'float', skiprows = 2, usecols = (0,1,2))
+file = np.loadtxt("../programs/build_b/run1e2", dtype = 'float', skiprows = 2, usecols = (0,1,2))
 
 file_lenght = str(len(file)-2)
 file_header = "Number of iterations " + file_lenght
@@ -16,12 +16,12 @@ fig = figure()
 grid(True, which = 'both')#, ls= '-')
 grid('on')
 
-plot(file[:,0],file[:,1],'r-', label = '$approx.\ solution$ ') 
-plot(file[:,0],file[:,2],'b-', label = '$exact\ solution$ ') 
+plot(file[:,0],file[:,1],'r-', label = '$Nummerical\ approx.$ ') 
+plot(file[:,0],file[:,2],'b-', label = '$Exact\ solution$ ') 
 
 xlabel(r'$x$', size = 20,labelpad= 5 )
-ylabel(r'$f(x)$',size = 20, labelpad = 5)
+ylabel(r'$u(x)$',size = 20, labelpad = 5)
 title(file_header)
 legend()
-fig.savefig('b_run10.png')
+fig.savefig('b-run1e2.png')
 show()
