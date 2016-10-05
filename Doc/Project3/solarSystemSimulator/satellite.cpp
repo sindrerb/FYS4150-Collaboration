7dafa5a2f4bf1a0b8cd413f88b562e861f567b91
heinehn <heinehn@student.matnat.uio.no>
2016-10-05 11:05:54 +0200

    updated satellite class

--- Doc/Project3/solarSystemSimulator/satellite.cpp
+++ Doc/Project3/solarSystemSimulator/satellite.cpp
@@ -4,3 +4,25 @@ Satellite::Satellite()
 {
 
 }
+
+void Satellite::setPos(double X,double Y,double Z){
+    Xpos = X;
+    Ypos = Y;
+    Zpos = Z;
+}
+
+void Satellite::setMass(double M){
+    Mass = M;
+
+}
+
+double Satellite::getRelativeDist(const Satellite ball){
+    double R,dX,dY,dZ;
+    dX = Xpos-ball.Xpos;
+    dY = Ypos-ball.Ypos;
+    dZ = Zpos-ball.Zpos;
+
+    R = sqrt(dX*dX + dY*dY + dZ*dZ)
+
+    return R;
+}
