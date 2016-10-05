53014c019e96285a6e5dbcbf9592535ff51eab4d
heinehn <heinehn@student.matnat.uio.no>
2016-10-05 10:35:28 +0200

    Changed folder location of class

---
+++ Doc/Project3/satelliteClass/satellite.h
@@ -0,0 +1,21 @@
+#ifndef SATELLITE_H
+#define SATELLITE_H
+#include <iostream>
+#include <matrix.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+typedef double real;
+
+class Satellite
+{
+private:
+
+public:
+    Satellite();
+    setMass();
+    setPosition();
+};
+
+#endif // SATELLITE_H
