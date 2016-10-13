c6ee117aeac342967b938128d7edc4a60f32b034
Sindre <sindrerb@student.matnat.uio.no>
2016-10-13 18:41:03 +0200

    Removed unneccecary functions and introduced global variables
    This is to ease up the simulation in the solarsystem class

--- Doc/Project3/src/satellite.h
+++ Doc/Project3/src/satellite.h
@@ -8,37 +8,23 @@
 
 class Satellite {
 public:
+    vec3 g_position;
+    vec3 g_velocity;
+    vec3 g_acceleration;
+    vec3 g_force;
+
     Satellite();
     Satellite(double mass, vec3 position, vec3 velocity);
 
-    double getSatelliteMass() const;
-    void setSatelliteMass(double mass);
-
-    const vec3 &getSatellitePosition() const;
-    void setSatellitePosition(const vec3 &position);
-
-    vec3 getSatelliteVelocity() const;
-    void setSatelliteVelocity(const vec3 &velocity);
-
-    vec3 getSatelliteAcceleration() const;
-    void setSatelliteAcceleration(const vec3 &acceleration);
-
-    static double getDistanceBetween(const Satellite &s1, const Satellite &s2 );
+    double getMass() const;
+    void setMass(double mass);
 
     double relativeDistanceTo(const Satellite &satelliteName );
-
-    void nextPositionEuler( vec3 initialPosition, vec3 initialVelocity, double timeStep );
-    void nextVelocityEuler( vec3 initialAcceleration, double timesStep );
-    void nextPositionVerlet( vec3 initialPosition, vec3 initialVelocity, vec3 initialAcceleration, double stimeStep );
-    void nextVelocityVerlet( vec3 initialAcceleration, double timesStep );
-    void nextAcceleration( Satellite satelliteName );
 
 private:
     double m_mass;
     vec3 m_position;
     vec3 m_velocity;
-    vec3 m_acceleration;
-    vec3 m_force;
 };
 
 #endif // SATELLITE_H
