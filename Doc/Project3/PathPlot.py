ebc9be12eaf672b459c6b911363c705732470436
vegro90 <tilrandom@gmail.com>
2016-10-14 11:28:58 +0200

    Updated directory structure
    Also updated .gitignore to ignore build dir's

--- Doc/Project3/PathPlot.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-earth = np.loadtxt("OUTPUT.txt", dtype = 'float', skiprows = 2, usecols=(4,5,6))
-
-plt.plot(earth[0],earth[1],'-')
-plt.show()
