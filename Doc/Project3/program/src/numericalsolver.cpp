79183ec84c3f319ce2e26eeb4d08fbfe226ddb2d
sindrerb <s.r.bilden@fys.uio.no>
2016-10-15 11:14:24 +0200

    Removed FLOPS by changing variables

--- Doc/Project3/program/src/numericalsolver.cpp
+++ Doc/Project3/program/src/numericalsolver.cpp
@@ -11,11 +11,11 @@ vec3 NumericalSolver::solveVerletPos(vec3 x, vec3 dx, vec3 ddx, double step) {
     return x +  (step * dx) + (step*step * ddx)/2.0;
 }
 
-vec3 NumericalSolver::solveVerletPos(vec3 x, vec3 dx, vec3 ddx, double step, double stepSquared) {
-    return x +  (step * dx) + (stepSquared * ddx)/2.0;
+vec3 NumericalSolver::solveVerletPos(vec3 x, vec3 dx, vec3 ddx, double step, double halfStepSquared) {
+    return x +  (step * dx) + halfStepSquared * ddx;
 }
 
-vec3 NumericalSolver::solveVerletVel(vec3 dx, vec3 ddx1, vec3 ddx0, double step) {
-    return dx + step*(ddx1+ddx0)/2.0;
+vec3 NumericalSolver::solveVerletVel(vec3 dx, vec3 ddx1, vec3 ddx0, double halfStep) {
+    return dx + halfStep*(ddx1+ddx0);
 }
 
--- Doc/Project3/program/src/numericalsolver.h
+++ Doc/Project3/program/src/numericalsolver.h
@@ -9,8 +9,8 @@ public:
 
     static vec3 solveEuler(vec3 x, vec3 dx, double step);
     static vec3 solveVerletPos(vec3 x, vec3 dx, vec3 ddx, double step);
-    static vec3 solveVerletPos(vec3 x, vec3 dx, vec3 ddx, double step, double stepSquared);
-    static vec3 solveVerletVel(vec3 dx, vec3 ddx1, vec3 ddx0, double step);
+    static vec3 solveVerletPos(vec3 x, vec3 dx, vec3 ddx, double step, double halfStepSquared);
+    static vec3 solveVerletVel(vec3 dx, vec3 ddx1, vec3 ddx0, double halfStep);
 
 private:
     NumericalSolver();
