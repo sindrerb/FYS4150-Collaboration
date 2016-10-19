c7ad3d5e64c9cf94486dc801136431470d6fa0e9
sindrerb <s.r.bilden@fys.uio.no>
2016-10-19 13:46:27 +0200

    Small unimportant changes and testfiles

--- Doc/Project3/program/PathPlot.py
+++ Doc/Project3/program/PathPlot.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = "OUTPUT.txt"
+filename = "solarPlot.txt"
 
-data = open("../debug/src/"+filename)
+data = open("./debug/src/"+filename)
 celestials = data.read().splitlines()[1]
 celestials = celestials.split()[1:]
 
-system = np.loadtxt("../debug/src/"+filename, dtype = 'float',delimiter=",", skiprows = 2)
+system = np.loadtxt("./debug/src/"+filename, dtype = 'float',delimiter=",", skiprows = 2)
 plt.scatter(0,0,c='y',s=50)
 for i in range(1,len(celestials)):
     plt.plot(system[:,3*i+1],system[:,3*i+2],':',label='%s' %(celestials[i]).title())
