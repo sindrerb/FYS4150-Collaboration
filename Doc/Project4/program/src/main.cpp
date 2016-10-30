e0a6106651b186cc282bfdb3f8472a0d14b1da07
sindrerb <s.r.bilden@fys.uio.no>
2016-10-30 16:15:37 +0100

    A main that calls Ising2D
     for different temperatures and writes the result to file

--- Doc/Project4/program/src/main.cpp
+++ Doc/Project4/program/src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "matrix.h"
 #include "mpi.h"
+#include <iostream>
 #include <fstream>
+#include <iomanip>
+#include <cstdlib>
 #include "ising2d.h"
 
 using namespace std;
@@ -9,19 +12,27 @@ using namespace std;
 // output file
 ofstream ofile;
 
+void output(int, int, double, double *);
+
 int main(int argc, char *argv[])
 {
     char *outfilename;
     srand(time(NULL));
     long random = rand(); //Random seed
-    int my_rank,n_processes;
-    Ising2D ising(2);
+    int n_spins,mcs,my_rank,n_processes;
+    double total_average[5],initial_temp,final_temp,temp_step;
 
+    n_spins = 2;
+    mcs = 100000;
+    initial_temp = 1;
+    final_temp = 2;
+    temp_step = 0.01;
+
+    Ising2D ising(n_spins);
     //  MPI initializations
     MPI_Init (&argc, &argv);
     MPI_Comm_size (MPI_COMM_WORLD, &n_processes);
     MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
-
     if (my_rank == 0 && argc <= 1) {
       cout << "Bad Usage: " << argv[0] <<
         " read output file" << endl;
@@ -31,14 +42,47 @@ int main(int argc, char *argv[])
       outfilename=argv[1];
       ofile.open(outfilename);
     }
+    double *average = new double[5];
+
+
+    //Divide cycles over the nodes
+    int no_intervalls = mcs/n_processes;
+    int myLoopStart = my_rank*no_intervalls + 1;
+    int myLoopEnd = (my_rank+1)*no_intervalls;
+    if ( (my_rank == n_processes-1) &&( myLoopEnd < mcs) ) myLoopEnd = mcs;
 
     srand(random-my_rank);
-    printf( "Running process %d of %d processes\n", my_rank, n_processes);
-    ising.InitializeRandomState();
-    //ising.InitializeGroundState();
 
-    ising.delteLattice();
-    MPI_Finalize();
+    for ( double temperature = initial_temp; temperature <= final_temp; temperature+=temp_step) {
+        ising.generate(myLoopStart,myLoopEnd,temperature);
+        average = ising.fetch();
+        for(int i = 0; i<5;i++){
+            MPI_Reduce(&average[i], &total_average[i], 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        }
+        if(my_rank == 0){
+            output(n_spins,mcs,temperature,total_average);
+        }
+    }
     ofile.close();
+    MPI_Finalize();
     return 0;
+}
+
+void output(int n_spins, int mcs, double temperature, double *total_average) {
+  double norm = 1/((double) (mcs));  // divided by total number of cycles
+  double Etotal_average = total_average[0]*norm;
+  double E2total_average = total_average[1]*norm;
+  double Mtotal_average = total_average[2]*norm;
+  double M2total_average = total_average[3]*norm;
+  double Mabstotal_average = total_average[4]*norm;
+  // all expectation values are per spin, divide by 1/n_spins/n_spins
+  double Evariance = (E2total_average- Etotal_average*Etotal_average)/n_spins/n_spins;
+  double Mvariance = (M2total_average - Mtotal_average*Mtotal_average)/n_spins/n_spins;
+  ofile << setiosflags(ios::showpoint | ios::uppercase);
+  ofile << setw(15) << setprecision(8) << temperature;
+  ofile << setw(15) << setprecision(8) << Etotal_average/n_spins/n_spins;
+  ofile << setw(15) << setprecision(8) << Evariance/temperature/temperature;
+  ofile << setw(15) << setprecision(8) << Mtotal_average/n_spins/n_spins;
+  ofile << setw(15) << setprecision(8) << Mvariance/temperature;
+  ofile << setw(15) << setprecision(8) << Mabstotal_average/n_spins/n_spins << endl;
 }
