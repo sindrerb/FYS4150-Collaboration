6f70b9a253cb84caf4c88124e5e3e66b4a29bbcb
sindrerb <s.r.bilden@fys.uio.no>
2016-11-04 17:46:36 +0100

    MPI

--- Doc/Project4/program/src/main.cpp
+++ Doc/Project4/program/src/main.cpp
@@ -1,3 +1,4 @@
+#include "mpi/mpi.h"
 #include "ising2d.h"
 #include <iostream>
 
@@ -5,16 +6,40 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    string filename = "test3";
-    int nSpin = 2;
-    long monteCarlo = 100000000;
-    double *myAverage;
-    double temp = 1;
+    string filename;
+    int numprocs, myRank, nSpin;
+    long monteCarloCycles;
+    double *myAverage, *totalAverage;
+    double initTemp, finalTemp, tempStep;
 
-    Ising2D test(nSpin);
+    // Initialize MPI
+    MPI_Init (&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+    if(myRank ==  0 && argc <= 6) {
+        cout << "Bad usage: " << argv[0] << ", read outputfile, number of spins, number of samples, initial temperature, final temp., temp. step.";
+        exit(1);
+    }
+    if(myRank == 0 && argc > 6) {
+        filename = argv[1];
+        nSpin = atoi(argv[2]);
+        monteCarloCycles = atol(argv[3]);
+        initTemp = atof(argv[4]);
+        finalTemp = atof(argv[5]);
+        tempStep = atof(argv[6]);
+    }
+    int intervals = monteCarloCycles/numprocs;
+    int myLoopStart = myRank*intervals+1;
+    int myLoopEnd = myRank*intervals;
+    if ((myRank == numprocs-1)&&(myLoopEnd<monteCarloCycles)) myLoopEnd = monteCarloCycles;
+    Ising2D ising(nSpin);
+    ising.InitializeLattice();
+    myAverage = ising.Metropolis(0,monteCarloCycles,initTemp);
 
-    test.InitializeLattice();
-    myAverage = test.Metropolis(0,monteCarlo,temp);
-    test.output(filename,monteCarlo,temp,myAverage);
-    test.delteLattice();
+    MPI_Reduce(&myAverage[5],&totalAverage,5,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+    if(myRank == 0) {
+        ising.output(filename,monteCarloCycles,initTemp,myAverage);
+    }
+    ising.delteLattice();
+    MPI_Finalize();
 }
--- Doc/Project4/program/src/src.pro
+++ Doc/Project4/program/src/src.pro
@@ -10,13 +10,13 @@ HEADERS += \
     ising2d.h
 
 # MPI Settings UBUNTU
-#QMAKE_CXX = mpicxx
-#QMAKE_CXX_RELEASE = $$QMAKE_CXX
-#QMAKE_CXX_DEBUG = $$QMAKE_CXX
-#QMAKE_LINK = $$QMAKE_CXX
-#QMAKE_CC = mpicc
+QMAKE_CXX = mpicxx
+QMAKE_CXX_RELEASE = $$QMAKE_CXX
+QMAKE_CXX_DEBUG = $$QMAKE_CXX
+QMAKE_LINK = $$QMAKE_CXX
+QMAKE_CC = mpicc
 
-#QMAKE_CFLAGS += $$system(mpicc --showme:compile)
-#QMAKE_LFLAGS += $$system(mpicxx --showme:link)
-#QMAKE_CXXFLAGS += $$system(mpicxx --showme:compile) -DMPICH_IGNORE_CXX_SEEK
-#QMAKE_CXXFLAGS_RELEASE += $$system(mpicxx --showme:compile) -DMPICH_IGNORE_CXX_SEEK
+QMAKE_CFLAGS += $$system(mpicc --showme:compile)
+QMAKE_LFLAGS += $$system(mpicxx --showme:link)
+QMAKE_CXXFLAGS += $$system(mpicxx --showme:compile) -DMPICH_IGNORE_CXX_SEEK
+QMAKE_CXXFLAGS_RELEASE += $$system(mpicxx --showme:compile) -DMPICH_IGNORE_CXX_SEEK
