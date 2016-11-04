6f78b78ec598de13d2d0ea74c9be60ddab742e79
sindrerb <s.r.bilden@fys.uio.no>
2016-11-04 17:09:07 +0100

    Corrected analytic results

--- Doc/Project4/program/plot.py
+++ Doc/Project4/program/plot.py
@@ -8,40 +8,46 @@ Created on Sun Oct 30 15:25:18 2016
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
 def Z(T):
-    return 4*np.cosh(8*T)+12
+    return (2*np.exp(-8/T)+2*np.exp(8/T)+12)
 
 def E(T):
-    return 32.0*(np.sinh(8*T))/Z(T)
+    return 16*(np.exp(-8/T)-np.exp(8/T))/Z(T)
 
-def CV(x):
-    return x**-2*(2*np.cosh(x)+1)/(np.cosh(x)+3)**2 #missing one Kb
+def E2(T):
+    return 128*(np.exp(-8/T)+np.exp(8/T))/Z(T)
 
-def chi(x):
-    return 40/x
+def CV(T):
+    return (E2(T)-E(T)**2)/(T**2)
+
+def M2(T):
+    return 32*(np.exp(8/T)+1)/Z(T)
+
+def absM(T):
+    return 8*(np.exp(8/T)+2)/Z(T)
+def chi(T):
+    return (M2(T)-absM(T)**2)/T
+
+print E(1)/4, CV(1)/4, 0, chi(1)/4, 0;
+
 
 """
-fig, ax = plt.subplots()
-x = np.linspace(0.1,10,100)
-plt.xlabel('Temp')
-plt.ylabel('Cv')
-plt.plot(x,CV(x))
-
-fig, ax = plt.subplots()
-x = np.linspace(0.1,10,100)
-plt.xlabel('Temp')
-plt.ylabel(r'$\chi$')
-plt.plot(x,chi(x))
-"""
-data = np.loadtxt("./debug/src/test.txt",usecols=(0,2))
+num = [-1.992, -1.9956, -1.99781, -1.997105, -1.9971527, -1.9971469,]
+MC  = [   1E1,     1E2,      1E3,       1E4,        1E5,        1E6,       1E7,    1E8]
+data = np.loadtxt("./debug/src/lattice20",usecols=(0,1))
 #data = np.loadtxt("/home/sindrerb/testIsingBuild/testIsing.txt",usecols=(0,1))
 
-print E(0)
+print data[1]
 
-x=np.linspace(data[0,0],data[-1,0])
-plt.plot(x,E(x),'--',label="An")
+
+plt.plot(MC,num,'--',color='black',label='Numerical')
+plt.plot(MC,an[0],':',color='black',label='analytical')
+plt.xlabel('Monte Carlo cycles')
+plt.ylabel(r'$\langle$E$\rangle$')
+plt.legend()
+#x=np.linspace(data[0,0],data[-1,0],1000)
+x = np.linspace(0,4,1000)
+#plt.plot(x,E(x),'--',label="An")
 
 #plt.plot(x,Energy2(x)*2/100000,'-.',label="An2")
 Cv = CV(x)
@@ -51,5 +57,6 @@ Chi = chi(x)
 
 plt.ylabel("Energy J")
 plt.xlabel("Temp. J/k")
-plt.plot(data[:,0],data[:,1],label="Numeric")
+#plt.plot(data[:,0],data[:,1],label="Numeric")
 plt.legend()
+"""
