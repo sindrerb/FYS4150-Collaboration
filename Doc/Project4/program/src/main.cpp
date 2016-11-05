d785568b154d47f1d1147f3fba7ba212b48c0e08
heinehn <heinehn@student.matnat.uio.no>
2016-11-05 14:53:49 +0100

    Changed to using pow command for exponent.

--- Doc/Project4/program/src/main.cpp
+++ Doc/Project4/program/src/main.cpp
@@ -1,23 +1,23 @@
 #include "ising2d.h"
 #include <string>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
     //Solving task 4b finding MC convergence for 2x2 spins
-    long monteCarlo = 10; //Start # MC cicles
-    int cicles = 10;
-    int N = 3;
+    long monteCarlo; //Start # MC cicles
+    int N = 9;
     int nSpin = 2;
-    for(int i = 1; i <= N; i++){
+    for(long i = 1; i <= N; i++){
         string filename = "test";
-        cout << i << setw(10) <<monteCarlo << endl;
-        for(int j = 1; j <= i; j++){
-            monteCarlo = monteCarlo*cicles;
-        }
 
+        for(int j = 1; j <= i; j++){
+            monteCarlo = pow(10,i);
+        }
+        cout << i << setw(10) <<monteCarlo << endl;
         double *myAverage;
         double temp = 1;
         Ising2D test(nSpin);
@@ -26,6 +26,6 @@ int main()
         myAverage = test.Metropolis(0,monteCarlo, temp);
         test.output(filename, i, monteCarlo, temp, myAverage);
         test.delteLattice();
-        cout << "test nr. " << i << " done." << endl;
+        cout << "test nr. " << i << " with " << monteCarlo << " cycles are done." << endl;
     }
 }
