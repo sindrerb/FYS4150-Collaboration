77ebcdc1479578ea37b32fcd673573e8576ea224
sindrerb <s.r.bilden@fys.uio.no>
2016-11-10 16:47:54 +0100

    Data and plotting programs
    Equilibrium and histogram for 1.0,1.7 and 2.4 T

--- Doc/Project4/program/equilibriumPlot.py
+++ Doc/Project4/program/equilibriumPlot.py
@@ -4,24 +4,32 @@ Created on Tue Nov  8 11:37:40 2016
 
 @author: sindrerb
 """
-filename = "./release/histogram/latt20eq"
 
-cols = [0,2]
+
+cols = [0,1,2,3,4,5,6]
 import matplotlib.pyplot as plt
 import numpy as np
 
-labels=np.loadtxt(filename,dtype='string', usecols=cols)[1,:]
-rnd = np.loadtxt(filename,dtype='float', skiprows=2,usecols=cols)
+
 #rnd = np.loadtxt("./benchmarks/convergence20G",dtype='float', skiprows=1,usecols=cols)
 
 fig = plt.figure()
-plt.title("Calculations from a random state at T=1")
+plt.title("Calculations from the ground state")
+temps = ['1_0','1_7','2_4']
+j = 4
+for i,T in enumerate(temps):
+    filename = "./release/histogram/eq20R"+T+"T"
+    
+    labels=np.loadtxt(filename,dtype='string', usecols=cols)[0,:]
+    rnd = np.loadtxt(filename,dtype='float', skiprows=2,usecols=cols)[:500]
+    
 
-for i in range(1,len(labels)):
-    plt.subplot(len(labels)-1,1,i)
+    plt.subplot(len(temps),1,i+1)
     #plt.ylabel(labels[i])
-    plt.ylim(min(rnd[:,i])-abs(min(rnd[:,i]))*0.01,max(rnd[:,i])+abs(max(rnd[:,i]))*0.01)
-    plt.plot(rnd[:,0],rnd[:,i])
+    #plt.ylim(min(rnd[:,j])-abs(min(rnd[:,j]))*0.1,max(rnd[:,j])+abs(max(rnd[:,j]))*0.1)
+    plt.plot(rnd[:,0],rnd[:,j],label=str(1+i*0.7))
+    plt.ylabel(labels[j])
+    plt.legend()
 #plt.xlabel(labels[0])
-plt.ylabel("Accepted tries")
-plt.xlabel("Total tries")
+
+plt.xlabel("Total cycles")
--- Doc/Project4/program/histPlot.py
+++ Doc/Project4/program/histPlot.py
@@ -8,13 +8,19 @@ Created on Wed Nov  9 08:50:14 2016
 import matplotlib.pyplot as plt
 import numpy as np
 
-dat = np.loadtxt("./release/histogram/latt20hist",skiprows=3)
 
-temp = dat[:,0]
-energy = dat[0,1:]
-hist = dat[1,1:]
-#plt.xticks(np.linspace(energy[0],energy[-1],len(energy)))
-plt.xlim(energy[hist>0][0],energy[hist>0][-1]+4)
-print max(hist)
-#plt.ylim(0,max(hist)*1.01)
-plt.step(energy,hist/sum(hist),where='mid')
+for i,L in enumerate(['1_7']):#,'1_0','2_4'
+    dat = np.loadtxt("./release/histogram/hist20G"+L+"T",skiprows=3)
+    temp = dat[1,0]
+    energy = dat[0,1:]
+    hist = dat[1,1:]
+    #plt.xticks(np.linspace(energy[0],energy[-1],len(energy)))
+    plt.xlim(energy[0],-650)
+    #plt.yscale('log')
+    #plt.ylim(0,max(hist)*1.01)
+    #plt.ylim(0,1)
+    plt.step(energy,hist/sum(abs(hist)),where='mid',label='T='+str(temp))
+
+plt.legend()
+plt.ylabel("Probability P(E)")
+plt.xlabel("Energy [J]")
